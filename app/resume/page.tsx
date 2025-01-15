cd47ab792cd2359a41f3f6f5788996525b27f149
Emma Fewer <emmafewer20@gmail.com>
2025-01-14 17:01:35 -0700

    build fail

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Card, CardBody } from "@nextui-org/card";
+import { Card, CardBody } from "@nextui-org/react";
 
 export default function Page() {
   return (
